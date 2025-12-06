54ce14d25f468f67274f9f9923911321bbcf0a53
Nana <alhayuna0@gmail.com>
2025-12-06 13:13:47 +0700

    06/12/25
    Fixed from katalog website.

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const ENV = {
+    API_URL: "https://gudang.skytama.com/api/public-barang",
+    IMAGE_BASE_URL: "https://gudang.skytama.com/storage/",
+    WHATSAPP_NUMBER: "6285608902857",
+    PLACEHOLDER_IMAGE: "https://via.placeholder.com/280x180?text=No+Image"
+};
